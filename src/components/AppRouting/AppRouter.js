0f95e9d6511e0ffcdf8abd733e3addc3667bd5e7
devlitus <developercarles@gmail.com>
2021-12-20 06:55:26 +0100

    ad new route

--- src/components/AppRouting/AppRouter.js
+++ src/components/AppRouting/AppRouter.js
@@ -1,13 +1,15 @@
-import { Routes,  Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { About } from "../../pages/About/About";
 import { Home } from "../../pages/Home/Home";
-import { Services } from "../../pages/Services/Services";
+import { Jobs } from "../../pages/Jobs/Jobs";
+import { Services } from "../../pages/ServicesAndProjects/Services";
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/service" element={<Services />} />
+      <Route path="/jobs" element={<Jobs />} />
     </Routes>
-  )
-}
+  );
+};
