56c31601cc76f869fc7683443c6cc3f9668f6268
devlitus <developercarles@gmail.com>
2021-12-20 06:59:00 +0100

    add tranlation page Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,38 +1,38 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarker } from '@fortawesome/free-solid-svg-icons';
-import { faAt } from '@fortawesome/free-solid-svg-icons';
-import { faMobileAlt } from '@fortawesome/free-solid-svg-icons';
-
+import { faAt, faMapMarker } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useTranslation } from "react-i18next";
+import image3 from "../../assets/images/05.jpg";
+import image1 from "../../assets/images/25.jpg";
+import image2 from "../../assets/images/27.jpg";
 import { Card } from "../../components/Card/Card";
-import { LayoutMovile } from "../../components/LayoutMovile/LayoutMovile";
-import image1 from '../../assets/images/25.jpg';
-import image2 from '../../assets/images/27.jpg';
-import image3 from '../../assets/images/05.jpg';
-import './Home.css';
+import { Map } from "../../components/Map/Map";
+import "./Home.css";
 
 export const Home = () => {
-
+  const { t } = useTranslation();
+  const { cite, auhtorCite, titleContat, contactP, contactP2, contactP3 } =
+    t("home");
   return (
-    <LayoutMovile>
+    <>
       <div className="home">
         <div className="bg-image"></div>
       </div>
       <div className="blockquote">
         <p className="blockquote__img"></p>
-        <p className="blockquote__text">“Tots els adults van ser alguna vegada nens… però només uns pocs ho recorden.”</p>
-        <p className="blockquote__author">Petit Princep</p>
+        <p className="blockquote__text">{cite}</p>
+        <p className="blockquote__author">{auhtorCite}</p>
       </div>
-      
+
       <div className="home__container">
         <h1>Falta Títol</h1>
         <div className="home__container--principal">
-          <Card image={ image1 }/>
-          <Card image={ image2 }/>
-          <Card image={ image3 }/>
+          <Card image={image1} />
+          <Card image={image2} />
+          <Card image={image3} />
         </div>
       </div>
       <div className="home__contact">
-        <h1>Contacta amb nosaltres</h1>
+        <h1>{t(titleContat)}</h1>
         <div className="home__contact--icons">
           <div className="home__contact--icon">
             <FontAwesomeIcon icon={faMapMarker} />
@@ -42,12 +42,18 @@ export const Home = () => {
             <FontAwesomeIcon icon={faAt} />
             <span>rogerllopart@rogerllopart.net</span>
           </div>
-          <div className="home__contact--icon">
-            <FontAwesomeIcon icon={faMobileAlt} />
-            <span>+34 649 156 328</span>
-          </div>
         </div>
       </div>
-    </LayoutMovile>
-  )
-}
+      <p style={{ textAlign: "center", margin: "0", padding: "0" }}>
+        {t(contactP)}
+      </p>
+      <p style={{ textAlign: "center", margin: "0", padding: "0" }}>
+        <strong>{t(contactP2)}</strong>
+      </p>
+      <p style={{ textAlign: "center", margin: "0", padding: "0" }}>
+        {t(contactP3)}
+      </p>
+      <Map />
+    </>
+  );
+};
