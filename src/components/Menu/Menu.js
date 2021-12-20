1ac49f4d992dc475f6c447d530137926ac324fc6
devlitus <developercarles@gmail.com>
2021-12-20 06:57:15 +0100

    add link route

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,16 +1,32 @@
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import { SelectLanguages } from "../SelectLanguage/SelectLanguage";
 import "./Menu.css";
 
-export const Menu = ({handlerSidebar}) => {
+export const Menu = ({ handlerSidebar }) => {
+  const { t } = useTranslation();
   const handlerClick = () => {
     handlerSidebar();
-  }
+  };
   return (
     <div className="menu">
+      <SelectLanguages onClick={handlerClick} />
       <nav className="menu__navigation">
-        <Link to="/" onClick={handlerClick}>Inici</Link>
-        <Link to="/about" onClick={handlerClick} >Roger Llopart</Link>
-        <Link to="/service" onClick={handlerClick}>Serveis i projectes</Link>
+        <Link to="/" onClick={handlerClick}>
+          {t("home.pathUrl")}
+        </Link>
+        <Link to="/about" onClick={handlerClick}>
+          {t("about.pathUrl")}
+        </Link>
+        <Link to="/service" onClick={handlerClick}>
+          {t("service.pathUrl")}
+        </Link>
+        <Link to="/jobs" onClick={handlerClick}>
+          {t("job.pathUrl")}
+        </Link>
+        <Link to="/campus" onClick={handlerClick}>
+          {t("campus.pathUrl")}
+        </Link>
       </nav>
     </div>
   );
