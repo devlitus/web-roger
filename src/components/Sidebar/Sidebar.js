6282b17954a1fd444132ca7bad7a2ff743c8a59c
devlitus <developercarles@gmail.com>
2021-12-26 09:51:12 +0100

    refactor sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,25 @@
-
-import 'animate.css';
-import './Sidebar.css';
+import "animate.css";
+import "./Sidebar.css";
 export const Sidebar = ({ children, isSidebarOpen }) => {
+  const addClass = () => {};
   return (
-    <div className={`sidebar ${isSidebarOpen ? 'animate__animated animate__fadeInRight' : 'animate__animated animate__fadeOutRight'}`}>
+    <div
+      className={`sidebar ${
+        isSidebarOpen
+          ? "animate__animated animate__fadeInRight"
+          : "animate__animated animate__fadeOutRight"
+      }`}
+      style={{
+        display: isSidebarOpen && "block",
+      }}
+    >
       {children}
     </div>
-  )
-}
+  );
+};
+
+/* style={{
+  display: isSidebarOpen ? "block" : "none",
+}} */
+// animate__animated animate__fadeInRight
+// animate__animated animate__fadeOutRight
