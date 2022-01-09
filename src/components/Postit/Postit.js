7918d6e8deae3f6dff2f9b376392f883bcc7283f
devlitus <developercarles@gmail.com>
2022-01-09 12:35:04 +0100

    Añadir un nuevo componente Postit y el css Postit

---
+++ src/components/Postit/Postit.js
@@ -0,0 +1,28 @@
+export const Postit = ({ text }) => {
+  return (
+    <div className="container">
+      <div className="container-inner">
+        <div className="sticky-container">
+          <div className="sticky-outer">
+            <div className="sticky">
+              <svg width="0" height="0">
+                <defs>
+                  <clipPath id="stickyClip" clipPathUnits="objectBoundingBox">
+                    <path
+                      d="M 0 0 Q 0 0.69, 0.03 0.96 0.03 0.96, 1 0.96 Q 0.96 0.69, 0.96 0 0.96 0, 0 0"
+                      strokeLinejoin="round"
+                      strokeLinecap="square"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+              <div className="sticky-content">
+                <span>{text}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,6 @@
 import { useTranslation } from "react-i18next";
 import infant from "../../assets/images/infant.jpg";
-import talk from "../../assets/images/talk.svg";
-import cone from "../../assets/images/cone.svg";
-import power from "../../assets/images/power.svg";
-import orga from "../../assets/images/orga.svg";
-import education from "../../assets/images/education.svg";
-import { Link } from "react-router-dom";
-import { Map } from "../../components/Map/Map";
+import { Postit } from "../../components/Postit/Postit";
 
 export const Home = () => {
   const { t } = useTranslation();
@@ -21,56 +15,36 @@ export const Home = () => {
       </div>
       <div className="wrapper">
         <div className="content">
-          <h1 className="content__title">El nostres Serveis</h1>
-          <div className="content__service">
-            <Link to="/service">
-              <div className="box">
-                <img className="box__image" src={talk} alt="" />
-                <p className="box__text">Xarrades i tellers</p>
-              </div>
-            </Link>
-            <Link to="/service">
-              <div className="box">
-                <img className="box__image" src={cone} alt="" />
-                <p className="box__text">Càpsulas de coneixements</p>
-              </div>
-            </Link>
-            <div className="box">
-              <img className="box__image" src={power} alt="" />
-              <p className="box__text">Autoritat sense autoritarisme</p>
-            </div>
-            <div className="box">
-              <img className="box__image" src={orga} alt="" />
-              <p className="box__text">Metodologia i Organització</p>
-            </div>
-            <div className="box">
-              <img className="box__image" src={education} alt="" />
-              <p className="box__text">La formació a mida | Bonificada</p>
+          <div className="content__bgPostit">
+            <h1>Serveis i projectes</h1>
+            <div className="content__postit">
+              <Postit text="Xerrades i tallers" />
+              <Postit text="Càpsules de coneixement" />
+              <Postit text="La Formació a mida Bonificada" />
             </div>
           </div>
-          <div className="contact">
-            <div className="bg-contact">
-              <div className="contact__content">
-                <h3 className="contact__title">Contacta amb nosaltres</h3>
-                <ol className="contact__list">
-                  <li className="contact__list__item">
-                    <a href="mailto:rogerllopart@rogerllopart.net">
-                      rogerllopart@rogerllopart.net
-                    </a>
-                  </li>
-                  <li className="contact__list__item">
-                    <a href="https://">youtube</a>
-                  </li>
-                  <li className="contact__list__item">
-                    <a href="https://">instagram</a>
-                  </li>
-                  <li className="contact__list__item">
-                    <a href="https://">linkedin</a>
-                  </li>
-                </ol>
-              </div>
-            </div>
-            <Map />
+        </div>
+      </div>
+      <div className="contact">
+        <div className="bg-contact">
+          <div className="contact__content">
+            <h3 className="contact__title">Contacta amb nosaltres</h3>
+            <ol className="contact__list">
+              <li className="contact__list__item">
+                <a href="mailto:rogerllopart@rogerllopart.net">
+                  rogerllopart@rogerllopart.net
+                </a>
+              </li>
+              <li className="contact__list__item">
+                <a href="https://">youtube</a>
+              </li>
+              <li className="contact__list__item">
+                <a href="https://">instagram</a>
+              </li>
+              <li className="contact__list__item">
+                <a href="https://">linkedin</a>
+              </li>
+            </ol>
           </div>
         </div>
       </div>
