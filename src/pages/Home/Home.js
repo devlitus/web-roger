2c3fd44fe3270cdb4d5b49d3ac498147cd8a1bfb
devlitus <developercarles@gmail.com>
2022-01-09 11:54:48 +0100

    Modificar parte de contacto en la página home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,26 +1,12 @@
-import { faAt } from "@fortawesome/free-solid-svg-icons";
-import {
-  faYoutube,
-  faInstagram,
-  faLinkedinIn,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslation } from "react-i18next";
-// import image3 from "../../assets/images/05.jpg";
-// import image1 from "../../assets/images/25.jpg";
-// import image2 from "../../assets/images/27.jpg";
 import infant from "../../assets/images/infant.jpg";
 import talk from "../../assets/images/talk.svg";
 import cone from "../../assets/images/cone.svg";
 import power from "../../assets/images/power.svg";
 import orga from "../../assets/images/orga.svg";
 import education from "../../assets/images/education.svg";
-import contact from "../../assets/images/contact.svg";
 import { Link } from "react-router-dom";
 import { Map } from "../../components/Map/Map";
-// import { Card } from "../../components/Card/Card";
-
-// import { Map } from "../../components/Map/Map";
 
 export const Home = () => {
   const { t } = useTranslation();
@@ -62,27 +48,29 @@ export const Home = () => {
               <p className="box__text">La formació a mida | Bonificada</p>
             </div>
           </div>
-          <div className="content__contact">
-            <h1 className="content__title">Contacta amb nosaltres</h1>
-            <div className="content__contact__box">
-              <img className="contact__image" src={contact} alt="" />
-              <div className="contact__item">
-                <FontAwesomeIcon icon={faAt} size="4x" />
-                <FontAwesomeIcon icon={faYoutube} size="4x" />
-                <FontAwesomeIcon icon={faInstagram} size="4x" />
-                <FontAwesomeIcon icon={faLinkedinIn} size="4x" />
-              </div>
-              <div className="contact__item">
-                <p className="contact__item__text">
-                  Estem al centre de Barcelona, al carrer Gran Via de de les
-                  Corts Catalanes 672, entre Passeig de Gràcia i Passeig de Sant
-                  Joan.
-                </p>
-              </div>
-              <div className="contact__item">
-                <Map />
+          <div className="contact">
+            <div className="bg-contact">
+              <div className="contact__content">
+                <h3 className="contact__title">Contacta amb nosaltres</h3>
+                <ol className="contact__list">
+                  <li className="contact__list__item">
+                    <a href="mailto:rogerllopart@rogerllopart.net">
+                      rogerllopart@rogerllopart.net
+                    </a>
+                  </li>
+                  <li className="contact__list__item">
+                    <a href="https://">youtube</a>
+                  </li>
+                  <li className="contact__list__item">
+                    <a href="https://">instagram</a>
+                  </li>
+                  <li className="contact__list__item">
+                    <a href="https://">linkedin</a>
+                  </li>
+                </ol>
               </div>
             </div>
+            <Map />
           </div>
         </div>
       </div>
