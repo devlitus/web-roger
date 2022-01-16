a350247d11aca054817832e94a3194d62f02dcf3
devlitus <developercarles@gmail.com>
2022-01-16 09:42:43 +0100

    remove layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-// import { Header } from "./components/Header/Header";
-import { AppRouter } from "./components/AppRouting/AppRouter";
-
-// import { Navbar } from "./components/Navbar/Navbar";
-import { Layout } from "./components/Layout/Layout";
-// import { Home } from "./pages/Home/Home";
-import "./App.css";
+import { AppRouter } from "./AppRouting/router";
+import { Navbar } from "./components/Navbar";
 export const App = () => {
   return (
     <>
-      <Layout>
-        <AppRouter />
-      </Layout>
+      <Navbar />
+      <AppRouter />
     </>
   );
 };
